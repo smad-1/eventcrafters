8e27d61a6773485835ad7f34281a68d7779fab47
ramis <ramisaudhi125@gmail.com>
2023-12-30 16:22:51 +0600

    payment and history

--- login.js
+++ login.js
@@ -123,8 +123,8 @@ app.get('/views/showlots.html', (req, res) => {
 app.get('/views/birthday_page.html', (req, res) => {
   res.sendFile(__dirname + '/views/birthday_page.html');
 });
-app.get('/views/decor.html', (req, res) => {
-  res.sendFile(__dirname + '/views/decor.html');
+app.get('/views/events.html', (req, res) => {
+  res.sendFile(__dirname + '/views/events.html');
 });
 app.get('/views/Payment.html', (req, res) => {
   res.sendFile(__dirname + '/views/Payment.html');
@@ -149,12 +149,26 @@ app.get('/views/mybookings.html', (req, res) => {
 app.get('/views/myrequests.html', (req, res) => {
   res.sendFile(__dirname + '/views/myrequests.html');
 });
-app.get('/views/Payment.html', (req, res) => {
-  res.sendFile(__dirname + '/views/Payment.html');
-});
+
 app.get('/views/updateform.html', (req, res) => {
   res.sendFile(__dirname + '/views/updateform.html');
 });
+app.get('/views/decor.html', (req, res) => {
+  res.sendFile(__dirname + '/views/decor.html');
+});
+app.get('/views/catering.html', (req, res) => {
+  res.sendFile(__dirname + '/views/catering.html');
+});
+app.get('/views/history.html', (req, res) => {
+  res.sendFile(__dirname + '/views/history.html');
+});
+app.get('/views/ownerhistory.html', (req, res) => {
+  res.sendFile(__dirname + '/views/ownerhistory.html');
+});
+app.get('/views/customerhistory.html', (req, res) => {
+  res.sendFile(__dirname + '/views/customerhistory.html');
+});
+
 
 //set app port
 
@@ -171,22 +185,20 @@ function getUsername(id) {
 }
 
 app.get('/lots', (req, res) => {
-  const { userID, price, capacity, area } = req.query;
+  const { userID, price, capacity, area, event } = req.query;
 
   console.log(userID + capacity + area);
   var descPrice, descCapacity;
-  if (price == false) descPrice = 'ASC';
+  if (price == 'false') descPrice = 'ASC';
   else descPrice = 'DESC';
 
-  if (capacity == false) descCapacity = 'ASC';
+  if (capacity == 'false') descCapacity = 'ASC';
   else descCapacity = 'DESC';
 
-  let orderbyclause = `capacity ${descCapacity}`;
-
   var query;
-  if (area) query = `SELECT * FROM rentlots where userID != ? AND area = '${area}' group by userID, lotID, area order by capacity ${descCapacity}`;
-  else query = `SELECT * FROM rentlots where userID != ? group by userID, lotID, area order by capacity ${descCapacity} `;
-
+  if (area) query = `SELECT * FROM rentlots where userID != ? AND area = '${area}' AND events = '${event}' group by userID, lotID, area order by price ${descPrice}, capacity ${descCapacity}`;
+  else query = `SELECT * FROM rentlots where userID != ? AND events = '${event}' group by userID, lotID, area order by price ${descPrice}, capacity ${descCapacity} `;
+  //capacity ${descCapacity}, 
   // Fetch data from the database
   conn.query(query, [userID], (err, results) => {
     if (err) throw err;
@@ -246,6 +258,28 @@ app.get('/myrequests', (req, res) => {
   });
 });
 
+app.get('/ownerhistory', (req, res) => {
+  const { userID } = req.query;
+  // Fetch data from the database
+  const query = 'SELECT distinct reservationID, reservations.lotID as lot, customerID, reservations.userID as owner, eventDate, activityID, startTime, endTime, catering, decor, Numberofpeople, reservations.Phone_no, reservations.Email, state FROM reservations, rentlots WHERE reservations.userID = rentlots.userID AND rentlots.lotID = reservations.lotID AND reservations.userID = ? AND state = "paid"';
+  conn.query(query, [userID], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.get('/customerhistory', (req, res) => {
+  const { userID } = req.query;
+  // Fetch data from the database
+  const query = 'SELECT distinct reservationID, reservations.lotID as lot, customerID, reservations.userID as owner, eventDate, activityID, startTime, endTime, catering, decor, Numberofpeople, rentlots.Phone_no, rentlots.Email, state FROM reservations, rentlots WHERE reservations.userID = rentlots.userID AND rentlots.lotID = reservations.lotID AND customerID = ? AND state="paid"';
+  conn.query(query, [userID], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
+
 app.get('/getname', (req, res) => {
   const { userID } = req.query;
 
@@ -283,19 +317,75 @@ app.get('/getFeedback', (req, res) => {
   });
 });
 
+app.get('/total', (req, res) => {
+  const { resID } = req.query;
+  // Fetch data from the database
+  conn.query(`SELECT calculateLotRent(${resID})+calculateDecor(${resID})+calculateCatering(${resID}) as total`, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.get('/capanddate', (req, res) => {
+  const { userID, lotID, cap } = req.query;
+  // Fetch data from the database
+
+  query = 'select capacity from rentlots where userID = ? AND lotID = ? group by userID, lotID';
+  conn.query(query, [userID, lotID], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+class payment {
+  static lot;
+  static owner;
+  static res;
+}
+
+
 app.post('/payment', encoder, function (req, res) {
 
-  console.log("ashe?");
+  if (req.body.lotID == null || req.body.ownerID == null || req.body.reservationID == null) {
+    //res.send("done");
+    var query;
+    let date = new Date();
+    if (req.body.paymentMethod == 'bkash') {
+      query = 'insert into payment (reservationID, method, amount, lotID, status, bkashno, paymentdate) values (?, ?, ?, ?, ?, ?, ?)';
+      conn.query(query, [payment.res, 'bkash', req.body.amount, payment.lot, 'not verified', req.body.bkashNumber, date]), function (error, results, fields) {
+        if (error) throw error;
+        else console.log("ok");
+      }
+    }
+    else {
+      query = 'insert into payment (reservationID, method, amount, lotID, status, cardno, cardexp, paymentdate) values (?, ?, ?, ?, ?, ?, ?, ?)';
+      conn.query(query, [payment.res, 'bkash', req.body.amount, payment.lot, 'not verified', req.body.cardNumber, req.body.expireNumber, date]), function (error, results, fields) {
+        if (error) throw error;
+        else console.log("ok");
+      }
+      res.redirect(`/views/welcome.html?userID=${user.curr_user}&username=${user.curr_name}`);
+      res.end();
+    }
+  }
+  else {
+    payment.lot = req.body.lotID;
+    payment.owner = req.body.ownerID;
+    payment.res = req.body.reservationID;
 
-  //console.log(amount + " " +name + " " +addr + " " +phone_no + " " +req.body.lot-id + " ");
-  res.redirect("/views/welcome.html");
+    const query = 'select customerID from reservations where reservationID = ?';
+    conn.query(query, [payment.res], function (error, results, fields) {
+      if (results.length > 0) {
+        //res.redirect("/views/welcome.html")
+        user.curr_user = results[0].customerID;
+        getUsername();
+      } else {
+        console.log("mistake");
+      }
+    });
+  }
 
-  /*const query = 'insert into payment (reservationID, method, amount, type, name, address, phone_no, lotID, status) values (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  conn.query(query, [resID, method, amount, type, name, addr, phone_no, req.body.lot-id, status ]), function (error, results, fields)
-  {
-      if(error) throw error;
-      else console.log("ok");
-  }*/
 });
 
 
@@ -343,6 +433,7 @@ app.post('/updatelot', encoder, function (req, res) {
     var info = req.body.info
     var activityList = req.body.activity;
     var area = req.body.area
+    var price = req.body.price
 
     console.log("eije list" + activityList);
 
@@ -353,8 +444,8 @@ app.post('/updatelot', encoder, function (req, res) {
 
         //console.log("main lotid: " + lotid + 1);
         if (activityList[i] != "null") {
-          conn.query('INSERT INTO rentlots (userID, lotID, First_name, Last_name, Email, Phone_no, Lot_Name, Capacity, events, Area, Address, Extra_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            [user.curr_user, savelotID.lotid, fName, lName, email, phone_no, lotname, capacity, activityList[i], area, addr, info], function (error, results, fields) {
+          conn.query('INSERT INTO rentlots (userID, lotID, First_name, Last_name, Email, Phone_no, Lot_Name, Capacity, events, Area, Address, Price, Extra_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [user.curr_user, savelotID.lotid, fName, lName, email, phone_no, lotname, capacity, activityList[i], area, addr, price, info], function (error, results, fields) {
               if (error) {
                 console.error('Submission failed:', error);
               } else {
@@ -386,8 +477,9 @@ app.post('/updatelot', encoder, function (req, res) {
 
 app.post('/deletelot', encoder, function (req, res) {
   if (req.body.userID == null || req.body.lotID == null) {
-    
+
     window.location.href(`/views/mylots.html?userID=${user.curr_user}`);
+    res.send("done");
     res.end();
   }
   else {
@@ -398,10 +490,15 @@ app.post('/deletelot', encoder, function (req, res) {
     const query = 'delete from rentlots where userID=? AND lotID = ?';
     conn.query(query, [req.body.userID, req.body.lotID], (err, results) => {
       if (err) throw err;
-      else console.log("Deleted successfully.");
+      else {
+        console.log("Deleted successfully.");
+
+      }
     });
 
   }
+
+  //res.status(200).send("OK: Deleted successfully");
 
 });
 
@@ -417,6 +514,7 @@ app.post('/rentplace', encoder, function (req, res) {
   var info = req.body.info
   var activityList = req.body.activity;
   var area = req.body.area
+  var price = req.body.price
 
   var lotid = 0;
 
@@ -447,8 +545,8 @@ app.post('/rentplace', encoder, function (req, res) {
         } else {
           console.log("main lotid: " + lotid + 1);
           if (activityList[i] != "null") {
-            conn.query('INSERT INTO rentlots (userID, lotID, First_name, Last_name, Email, Phone_no, Lot_Name, Capacity, events, Area, Address, Extra_info, createdOn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-              [uid, lotid + 1, fName, lName, email, phone_no, lotname, capacity, activityList[i], area, addr, info, date], function (error, results, fields) {
+            conn.query('INSERT INTO rentlots (userID, lotID, First_name, Last_name, Email, Phone_no, Lot_Name, Capacity, events, Area, Address, Price, Extra_info, createdOn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+              [uid, lotid + 1, fName, lName, email, phone_no, lotname, capacity, activityList[i], area, addr, price, info, date], function (error, results, fields) {
                 if (error) {
                   console.error('Submission failed:', error);
                 } else {
@@ -462,6 +560,7 @@ app.post('/rentplace', encoder, function (req, res) {
       });
     }
   }
+
   res.redirect("/views/welcome.html" + `?userID=${user.curr_user}&username=${user.curr_name}`);
   res.end();
 
@@ -482,8 +581,9 @@ app.post('/bookplace', encoder, function (req, res) {
   var email = req.body.email
   var phone_no = req.body.number
   var noofpeople = req.body.capacity
-  var decor = req.body.decor
-  var catering = req.body.catering
+  var decor = req.body.senddec
+  var catering = req.body.sendcat
+  var type = req.body.sendtype
   var resDate = req.body.date
   var sTime = req.body.stime
   var eTime = req.body.etime
@@ -498,20 +598,17 @@ app.post('/bookplace', encoder, function (req, res) {
     + date.toTimeString().split(' ')[0]);
 
   //console.log(fName + " " + lName + " " + email + " " + phone_no + " " + lotID + " " + noofpeople + " " + decor + " " + catering+ " " + resDate+ " " + actID + " " + ownerID+ " " +sTime+ " " +eTime);
-
-
-
-  const query = 'INSERT INTO reservations (customerID, userID, lotID, activityID, First_name, Last_name, Email, Phone_no, Numberofpeople, resTime, eventDate, startTime, endTime, catering, decor, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  conn.query(query, [user.curr_user, ownerID, lotID, actID, fName, lName, email, phone_no, noofpeople, date, resDate, sTime, eTime, catering, decor, "requested"], (error, results) => {
+  const query = 'INSERT INTO reservations (customerID, userID, lotID, activityID, First_name, Last_name, Email, Phone_no, Numberofpeople, resTime, eventDate, startTime, endTime, catering, type, decor, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  conn.query(query, [user.curr_user, ownerID, lotID, actID, fName, lName, email, phone_no, noofpeople, date, resDate, sTime, eTime, catering, type, decor, "requested"], (error, results) => {
     if (error) {
       console.log("Submission failed: " + error);
     }
     else console.log("Successful submission");
   });
 
-  res.redirect("/views/welcome.html" + `?userID=${user.curr_user}&username=${user.curr_name}`);
+  //res.redirect("/views/welcome.html" + `?userID=${user.curr_user}&username=${user.curr_name}`);
 
-  res.end();
+  //res.end();
 
 });
 
